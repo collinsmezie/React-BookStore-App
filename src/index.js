3029534b215c7edff78afce74561b9f2f2cc5851
collinsmezie <collinschimezieobinwa@gmail.com>
2022-05-17 17:09:28 +0400

    add redux to bookstore project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </Provider>,
+);
